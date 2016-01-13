b445b68330a690c61f9f0150e1de52d216cd404a
devprashant <devprashantdawar@gmail.com>
2016-01-13 14:25:31 +0530

    structuring

---
+++ app/src/main/java/com/example/probook/stock/view/ObjectListFragment.java
@@ -0,0 +1,29 @@
+package com.example.probook.stock.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.probook.stock.R;
+
+/**
+ * Created by probook on 1/13/2016.
+ */
+public class ObjectListFragment extends Fragment {
+
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_collection_object, container,false);
+        Bundle args = getArguments();
+
+        ((TextView) rootView.findViewById(R.id.text1))
+                .setText(args.getString(ARG_OBJECT));
+
+        return rootView;
+    }
+}
