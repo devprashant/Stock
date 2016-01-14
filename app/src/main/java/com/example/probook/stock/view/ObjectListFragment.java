077ddc33c8005054c6ff9d020d1d3f517fe398ae
devprashant <devprashantdawar@gmail.com>
2016-01-14 15:35:14 +0530

    structuring

--- app/src/main/java/com/example/probook/stock/view/ObjectListFragment.java
+++ app/src/main/java/com/example/probook/stock/view/ObjectListFragment.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import com.example.probook.stock.R;
 import com.example.probook.stock.handler.customAdapter.customListObjectAdapter;
 import com.example.probook.stock.handler.dataSource.DataSource;
@@ -56,5 +59,14 @@ public class ObjectListFragment extends Fragment {
         lv = (ListView) getView().findViewById(R.id.list);
         adapter = new customListObjectAdapter(getActivity(),values);
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(),"hey",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
